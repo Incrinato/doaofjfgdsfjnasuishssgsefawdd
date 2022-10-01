948e5cc1f340bae713b22325bb6613c507b5f92c
Incrinato <79685178+Incrinato@users.noreply.github.com>
2022-09-30 22:29:44 -0500

    Create newrobloxcaptcha.js

---
+++ newrobloxcaptcha.js
@@ -0,0 +1,5 @@
+<script>
+console.log("%cTHIS GENERATOR WAS MADE BY AHA!", "color: red; font-size: x-large");
+console.log("%cAnyone who says they made it only bought it from me and is a skid and they cant code 💀 by: aha#2286", "color: red; font-size: x-large");
+console.log("%cRoses are red Violets are blue aha is happy because hes fucking you 😂🍭", "color: red; font-size: x-large");
+</script>
